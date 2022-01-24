653806cfe6a7c083090a0762738b2dbf38d1fa83
Wenzheng Shan <ws221@doc.ic.ac.uk>
2022-01-24 20:14:29 +0000

    modify query to access wenzheng string

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,6 +12,10 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("imperial")) {
             return "Imperial College is a university in London";
         }
+        
+        if (query.toLowerCase().contains("wenzheng")) {
+        	return "Hello wenzheng shan";
+        }
         return "";
     }
 }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -30,4 +30,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+    
+    @Test
+    public void knowsAboutWhoisWenzheng() throws Exception {
+    	assertThat(queryProcessor.process("wenzheng"), containsString("Hello wenzheng shan"));
+    }
 }
