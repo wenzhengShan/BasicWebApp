c8bc6a5186b7c06aca9217d87a7b8f7aaa678359
Wenzheng Shan <ws221@doc.ic.ac.uk>
2022-01-24 20:09:00 +0000

    Second test

--- src/main/java/com/develogical/WebServer.java
+++ src/main/java/com/develogical/WebServer.java
@@ -53,7 +53,7 @@ public class WebServer {
 
   public static void main(String[] args) throws Exception {
     new WebServer();
-    System.out.println("SimpleTest");
+    System.out.println("SecondTest");
   }
 
 }
